345d9b8a904fd25c990ad39b67e2e7f74ff06fd2
Philemon Eichin <phileicht@gmx.de>
2018-03-12 17:16:16 +0100

    Add IIndexerContextProvider

--- phirSOFT.ContextProperties/IContextProvider.cs
+++ phirSOFT.ContextProperties/IContextProvider.cs
@@ -5,4 +5,10 @@
         TValue GetValue(object targetObject, ContextProperty<TPropertyValue> targetProperty);
         bool OverridesValue(object targetObject, ContextProperty<TPropertyValue> targetProperty);
     }
+
+    public interface IIndexerContextProvider<out TValue, TKey, TPropertyValue> where TValue : TPropertyValue
+    {
+        IIndexer<TKey, TValue> GetValue(object targetObject, IndexerContextProperty<TKey, TPropertyValue> targetProperty);
+        bool OverridesValue(object targetObject, IndexerContextProperty<TKey, TPropertyValue> targetProperty);
+    }
 }
