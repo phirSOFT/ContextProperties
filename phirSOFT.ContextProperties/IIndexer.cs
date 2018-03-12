7d474510fb9d0080f990d100875b07fb4371cd6b
Philemon Eichin <phileicht@gmx.de>
2018-03-12 17:08:46 +0100

    Add IIndexer

---
+++ phirSOFT.ContextProperties/IIndexer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace phirSOFT.ContextProperties
+{
+    /// <summary>
+    /// Redirects an index operation
+    /// </summary>
+    /// <typeparam name="TKey">The key of the index</typeparam>
+    /// <typeparam name="TValue">The value of the index</typeparam>
+    public interface IIndexer<in TKey, out TValue>
+    {
+        /// <summary>
+        /// Gets the value associated with a given key.
+        /// </summary>
+        /// <param name="key">The key of the value to retrive.</param>
+        /// <returns>The value associated with the key.</returns>
+        TValue this[TKey key] { get; }
+    }
+}
